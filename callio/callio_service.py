470708c334e18a0d7d30cbde3e93f74f078269ee
HM <hieumdd@gmail.com>
2022-02-24 21:02:51 +0700

    feat: Updated callio_service

--- callio/callio_service.py
+++ callio/callio_service.py
@@ -1,19 +1,34 @@
-from ctypes import util
+from typing import Any, Callable
+from datetime import datetime
+
 from callio import callio_repo, call
+from db.bigquery import load, update
 from utils import utils
 
 
-def pipeline_service(table, get, transform, load):
+def pipeline_service(
+    table: str,
+    get: Callable[[datetime, datetime], list[dict[str, Any]]],
+    transform: Callable[[list[dict[str, Any]]], list[dict[str, Any]]],
+    load: Callable[[str, Callable[[list[str], str], None]], int],
+    id_key: list[str] = ["_id"],
+    time_key: str = "create_time",
+):
     def _svc(start, end):
         with callio_repo.get_session() as session:
-            data = utils.compose(transform, get)(session, start, end)
+            data = utils.compose(
+                load(table, update(id_key, time_key)),
+                transform,
+                get,
+            )(session, start, end)
         return data
 
     return _svc
 
 
 call_inbound_service = pipeline_service(
+    "CallInbound",
     callio_repo.get_listing("call", {"direction": 1}),
     call.transform,
-
+    load(call.schema),
 )
--- db/bigquery.py
+++ db/bigquery.py
@@ -7,33 +7,34 @@ BQ_CLIENT = bigquery.Client()
 DATASET = ""
 
 
-def load(
-    table: str,
-    schema: list[dict[str, Any]],
-    update_fn: Callable[[str], None] = None,
-):
-    def _load(data: list[dict[str, Any]]) -> int:
-        output_rows = (
-            BQ_CLIENT.load_table_from_json(
-                data,
-                f"{DATASET}_{table}",
-                job_config=bigquery.LoadJobConfig(
-                    create_disposition="CREATE_IF_NEEDED",
-                    write_disposition="WRITE_APPEND" if update_fn else "WRITE_TRUNCATE",
-                    schema=schema,
-                ),
+def load(schema: list[dict[str, Any]]):
+    def _load(table: str, update_fn: Callable[[str], None] = None):
+        def __load(data: list[dict[str, Any]]) -> int:
+            output_rows = (
+                BQ_CLIENT.load_table_from_json(
+                    data,
+                    f"{DATASET}_{table}",
+                    job_config=bigquery.LoadJobConfig(
+                        create_disposition="CREATE_IF_NEEDED",
+                        write_disposition="WRITE_APPEND"
+                        if update_fn
+                        else "WRITE_TRUNCATE",
+                        schema=schema,
+                    ),
+                )
+                .result()
+                .output_rows
             )
-            .result()
-            .output_rows
-        )
-        if update_fn:
-            update_fn(table)
-        return output_rows
+            if update_fn:
+                update_fn(table)
+            return output_rows
+
+        return __load
 
     return _load
 
 
-def update(p_key: str, time_key: str):
+def update(id_key: list[str], time_key: str):
     def _update(table: str):
         BQ_CLIENT.query(
             f"""
@@ -42,7 +43,7 @@ def update(p_key: str, time_key: str):
         FROM (
             SELECT
                 *,
-                ROW_NUMBER() OVER (PARTITION BY {','.join(p_key)} ORDER BY {time_key} DESC) AS row_num,
+                ROW_NUMBER() OVER (PARTITION BY {','.join(id_key)} ORDER BY {time_key} DESC) AS row_num,
             FROM {DATASET}_{table}
         ) WHERE row_num = 1
         """
