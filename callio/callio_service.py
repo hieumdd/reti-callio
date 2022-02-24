52724e7a9844cde89ef924d2bdd4811b4ec15cf2
HM <hieumdd@gmail.com>
2022-02-24 09:15:48 +0700

    feat: Added callio_service

--- callio/callio_service.py
+++ callio/callio_service.py
@@ -1,13 +1,19 @@
 from ctypes import util
-from callio import callio_repo
+from callio import callio_repo, call
 from utils import utils
 
-def pipeline_service(get, transform):
+
+def pipeline_service(table, get, transform, load):
     def _svc(start, end):
         with callio_repo.get_session() as session:
-            data = utils.compose(
-                transform,
-                get
-            )(session, start, end)
+            data = utils.compose(transform, get)(session, start, end)
         return data
+
     return _svc
+
+
+call_inbound_service = pipeline_service(
+    callio_repo.get_listing("call", {"direction": 1}),
+    call.transform,
+
+)
