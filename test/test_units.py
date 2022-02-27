1b50ae5880b3bcf2f33d41deb9ca7a2fa2f4d3b7
HM <hieumdd@gmail.com>
2022-02-27 21:24:23 +0700

    ci: Updated test_units

--- test/test_units.py
+++ test/test_units.py
@@ -2,35 +2,28 @@ import pytest
 
 from callio import callio_service
 
+
 @pytest.fixture(
     params=[
-        (None, None),
-        ('2022-01-02', '2022-01-10'),
+        # (None, None),
+        ("2022-01-01", "2022-03-01"),
     ],
     ids=[
-        "auto",
+        # "auto",
         "manual",
     ],
 )
 def timeframe(request):
     return request.param
 
+
 class TestCallio:
     @pytest.mark.parametrize(
         "service",
-        [
-            callio_service.call_inbound_service,
-            callio_service.call_outbound_service,
-            callio_service.call_internal_service,
-            callio_service.contact_service,
-        ],
-        ids=[
-            "CallInbound",
-            "CallOutbound",
-            "CallInternal",
-            "Contact",
-        ]
+        callio_service.services.values(),
+        ids=callio_service.services.keys(),
     )
     def test_service(self, service, timeframe):
         res = service(*timeframe)
+        print(res)
         res
